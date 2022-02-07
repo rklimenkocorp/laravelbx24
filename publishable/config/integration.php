c5df82843f3cb696a7c6fc7e704f6147ca761d98
Roman <rklimenko.corp@gmail.com>
2022-02-07 21:06:00 +0300

    Update integration.php

--- publishable/config/integration.php
+++ publishable/config/integration.php
@@ -4,4 +4,6 @@ return [
     'CRM_HOST' => env('MIND4ME_HOST'),
     'CLIENT_ID' => env('MIND4ME_CLIENT_ID'),
     'CLIENT_SECRET' => env('MIND4ME_CLIENT_SECRET'),
+    'USER' => env('MIND4ME_USER_LOGIN'),
+    'PASSWORD' => env('MIND4ME_USER_PASSWORD'),
 ];
